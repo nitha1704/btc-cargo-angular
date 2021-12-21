edb1f1e56714ece8186928d92a0a9438807ca3b2
Jamie Grover <alive1704@gmail.com>
2021-12-21 20:32:01 +0700

    add google authen

--- src/app/custom/social-login/google-login/google-login.component.ts
+++ src/app/custom/social-login/google-login/google-login.component.ts
@@ -1,18 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { noUndefined } from '@angular/compiler/src/util';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { BehaviorSubject, ReplaySubject } from 'rxjs';
 
 @Component({
-  selector: 'app-google-login',
-  templateUrl: './google-login.component.html',
-  styleUrls: ['./google-login.component.css']
+  selector: "app-google-login",
+  templateUrl: "./google-login.component.html",
+  styleUrls: ["./google-login.component.css"],
 })
 export class GoogleLoginComponent implements OnInit {
+  private auth2: gapi.auth2.GoogleAuth;
+  private subject = new ReplaySubject<gapi.auth2.GoogleUser>(1);
 
-  constructor() { }
+  constructor(private ref: ChangeDetectorRef) {}
 
   ngOnInit(): void {
+    this.googleAuthInit();
   }
 
-  googleLogin(): void {
-    alert('googlelogin')
+  googleAuthInit(): void {
+    gapi.load("auth2", () => {
+      this.auth2 = gapi.auth2.init({
+        client_id:
+          "652626006625-1oe3ac9rp86ug4npolkl6ob5do03as7v.apps.googleusercontent.com",
+      });
+    });
+    this.ref.detectChanges();
+  }
+
+  googleSignIn(): void {
+    this.auth2
+      .signIn({
+        scope: "https://www.googleapis.com/auth/gmail.readonly",
+      })
+      .then((user) => {
+        this.subject.next(user);
+        console.log(user);
+      })
+      .catch((err) => {
+        this.subject.next(null);
+      });
+  }
+
+  googleSignOut(): void {
+    this.auth2.signOut().then(() => {
+      this.subject.next(null);
+    });
   }
 }
